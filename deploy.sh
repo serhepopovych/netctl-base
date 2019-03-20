d9e7e2c8648c8ca2a38d3593cb4a67e9e565038c
Serhey Popovych <serhe.popovych@gmail.com>
2019-03-20 15:50:54 +0200

    netctl-base: Reimplement deploy.sh to simplify it
    This is basically wrapper around install.sh intended to provide sane
    defaults that can be overwritten by command line arguments and prepare
    environment variables used by install.sh.
    
    We should not perform extra checks nor adjusting environment variables
    as all necessary work is done by install.sh.
    
    By default, when no command line options are given instal.sh will be
    called to install system wide.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/sh -e
 
-# Rquires: mkdir(1), rm(1), mktemp(1)
-
-# Use KEEP_LOG=1 to keep installation logs in ".install" and entire <deploy_dir>
-# on failure to install to temporary location.
+# Rquires: mkdir(1), rm(1)
 
 ################################################################################
-
-prog_name="${0##*/}"
 
 # Usage: msg <fmt> ...
 msg()
@@ -33,47 +28,30 @@ abort()
 	exit $rc
 }
 
-# Usage: exec_vars() [NAME=VAL...] [--] <command> [<args>...]
-exec_vars()
-{
-	local func="${FUNCNAME:-exec_vars}"
-
-	# Bash and dash behaves differently when calling
-	# with variable preset: use function with local
-	# variables to handle both.
-	while [ $# -gt 0 ]; do
-		case "$1" in
-			*=*)  eval "local '$1'; export '${1%%=*}'" ;;
-			--)   shift; break ;;
-			*)    break ;;
-		esac
-		shift
-	done
-
-	eval "$@"
-}
-
 # Usage: usage
 usage()
 {
 	local rc=$?
 	printf -- '
-Usage: %s -s <deploy_git> [-d <deploy_dir>] [ -t <dir> ] [-o] [-b <ext>] [-h|-u]
+Usage: %s [-s <git_dir>] [-r <root_dir>] [-d <dest_dir>] [ -t <trgt_dir> ]
+          [-b <ext>] [-o] [-h|-u]
 where
-    -s <deploy_git> - directory with deploy project or "" (empty) to try
-                      this script directory ("%s")
-    -d <deploy_dir> - install destination directory where "root" and "dest"
-                      subdirs will be created (default: mktemp -d "%s.XXXXXXXX")
-    -t <dir>        - absolute path prefix on target system to the installed
-                      files (default: "%s")
-    -o              - force install to skip privileged parts like user account
-                      creation even if running as superuser (default: no)
-    -b <ext>        - backup existing destination regular file or symlink when
-                      exists by appending .<ext>ension to entry name on rename;
-                      skip on failure (default: disabled, <ext> is "inst-sh")
-    -h|-u           - display this help message
+    -s <git_dir>  - directory with project to deploy or "" (empty) to try this
+                    script directory ("%s")
+    -r <root_dir> - root directory to install (default: "%s")
+    -d <dest_dir> - destination directory under <root_dir> where to install
+                    project (default: "%s")
+    -t <trgt_dir> - absolute path prefix on target system to the installed
+                    files (default: "%s")
+    -b <ext>      - backup existing destination regular file or symlink when
+                    exists by appending .<ext>ension to entry name on rename;
+                    skip on failure (default: disabled, <ext> is "%s")
+    -o            - force install to skip privileged parts like user account
+                    creation even if running as superuser (default: no)
+    -h|-u         - display this help message
 
-' "$prog_name" "$SOURCE" "$NAME" "$TARGET"
+' "$prog_name" "$DFLT_SOURCE" "$DFLT_ROOT" "$DFLT_ROOT$DFLT_DEST" "$DFLT_TARGET" \
+  "$BACKUP_EXT"
 	exit $rc
 }
 
@@ -83,59 +61,71 @@ prog_name="${0##*/}"
 # Verbosity: log only fatal errors
 [ "$V" -ge 0 -o "$V" -le 0 ] 2>/dev/null || V=0
 
-## Try to determine SOURCE
-SOURCE="${0%/*}"
+## Parse command line and prepare environment variables
+
+# Try to determine THIS_DIR
+THIS_DIR="${0%/*}"
 # Make it absolute path
-SOURCE="$(cd "$SOURCE" && echo "$PWD")" &&
-[ "$SOURCE/deploy.sh" -ef "$0" ] ||
+THIS_DIR="$(cd "$THIS_DIR" && echo "$PWD")" &&
+[ "$THIS_DIR/deploy.sh" -ef "$0" ] ||
 	abort 'cannot find directory containing this script\n'
-NAME="${SOURCE##*/}"
 
-TARGET='/'
+# Sane defaults
+DFLT_SOURCE="${THIS_DIR}"
+DFLT_ROOT='/'
+DFLT_DEST=''
+DFLT_TARGET="${DFLT_DEST}"
 
-## Parse command line arguments
-deploy_git=''
-deploy_sys="$TARGET"
-deploy_ord=''
-deploy_bak=''
-while getopts 's:d:t:ob:hu' c; do
+# Export variables to environment
+export SOURCE="${DFLT_SOURCE}"
+export ROOT="${DFLT_ROOT}"
+export DEST="${DFLT_DEST}"
+export TARGET=''
+export BACKUP=''
+
+BACKUP_EXT='inst-sh'
+ORDINARY=''
+
+# Parse command line options
+while getopts 's:r:d:t:b:ohu' c; do
 	case "$c" in
-		s) deploy_git="-$OPTARG" ;;
-		d) deploy_dir="$OPTARG" ;;
-		t) deploy_sys="$OPTARG" ;;
-		o) deploy_ord=y ;;
-		b) deploy_bak="${OPTARG:-inst-sh}" ;;
+		s) SOURCE="$OPTARG" ;;
+		r) ROOT="${OPTARG:?-r missing or empty argument <root_dir>}" ;;
+		d) DEST="$OPTARG" ;;
+		t) TARGET="$OPTARG" ;;
+		b) BACKUP="${OPTARG:-$BACKUP_EXT}" ;;
+		o) ORDINARY=y ;;
 		h|u) usage ;;
 		*) ! : || usage ;;
 	esac
 done
+unset c
 
-# no extra arguments
+# No extra arguments
 shift $((OPTIND - 1))
 [ $# -eq 0 ] || usage
 
-# mandatory argument
-[ -n "$deploy_git" ] || usage
-deploy_git="${deploy_git#-}"
+SOURCE="${SOURCE:-$THIS_DIR}"
+INSTALL_SH="$SOURCE/install.sh"
+[ -e "$SOURCE/.git" -a -e "$INSTALL_SH" ] || \
+	abort '"%s" is not a <git_dir> directory\n' "$SOURCE"
 
-# -s <deploy_git>
-deploy_git="${deploy_git:-$SOURCE}"
-deploy_install_sh="$deploy_git/install.sh"
-[ -e "$deploy_git/.git" -a -e "$deploy_install_sh" ] || \
-	abort '"%s" is not a <deploy_git> directory\n' "$deploy_git"
+if [ -n "$ORDINARY" ]; then
+	# Reserved value for uid/gid is -1 as per
+	# kernel/sys.c::setresuid() syscall.
+	#
+	# Use 0xffffffff as -1 as $(printf '%#x' -1) will
+	# give 64-bit value while uid/gid are 32-bit.
+	RSVD_UGID=0xffffffff
 
-# -d <deploy_dir>
-if [ -n "$deploy_dir" ]; then
-	[ -d "$deploy_dir" -o ! -e "$deploy_dir" ] || \
-		abort '"%s" exists and not a directory\n' "$deploy_dir"
-	deploy_dir_is_temp=
-else
-	deploy_dir="$(mktemp -d "$NAME.XXXXXXXX")" || \
-		abort 'fail to make temporary directory for install\n'
-	deploy_dir_is_temp=y
+	export INSTALL_EUID=$RSVD_UGID
+	export INSTALL_EGID=$RSVD_UGID
 fi
 
-## Prepare cleanup
+export V=$V
+
+## Install
+
 exit_handler()
 {
 	local rc=$?
@@ -146,44 +136,14 @@ exit_handler()
 		msg 'success\n'
 
 		# Report install location: can be used by package manager
-		exec_vars V=1 -- msg "'deploy_dir:%s\n'" "'$deploy_dir'"
-
-		deploy_dir="$DEST/.install"
-	elif [ -n "$deploy_dir_is_temp" ]; then
-		msg 'failure\n'
+		local V=1
+		msg 'ROOT:%s\n' "$ROOT"
 	else
-		deploy_dir=
-	fi
-
-	if [ -z "$KEEP_LOG" -a -n "$deploy_dir" ]; then
-		msg 'cleaning up (remove "%s")\n' "$deploy_dir"
-		rm -rf "$deploy_dir" ||:
+		msg 'failure\n'
 	fi
 }
 trap exit_handler EXIT
 
-## Install deploy
-msg 'installing to "%s"\n' "$deploy_dir"
+msg 'installing to "%s"\n' "$ROOT"
 
-export ROOT="$deploy_dir/root"
-export DEST="$deploy_dir/dest"
-export TARGET="$deploy_sys"
-
-mkdir -p "$ROOT" "$DEST" || \
-	abort 'fail to make "root" and "dest" subdirs under "%s"\n' \
-		"$deploy_dir"
-
-# Reserved value for uid/gid is -1 as per
-# kernel/sys.c::setresuid() syscall.
-#
-# Use 0xffffffff as -1 as $(printf '%#x' -1) will
-# give 64-bit value while uid/gid are 32-bit.
-RSVD_UGID=0xffffffff
-
-exec_vars V=$V ${deploy_ord:+INSTALL_EUID=$RSVD_UGID INSTALL_EGID=$RSVD_UGID} \
-	BACKUP="$deploy_bak" EEXIST='' -- \
-	"$deploy_install_sh" || \
-	abort 'fail to install deploy using "%s" to "%s"\n' \
-		"$deploy_install_sh" "$deploy_dir"
-
-exit 0
+"$INSTALL_SH"
