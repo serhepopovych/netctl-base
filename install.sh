serhepopovych/netctl-base

commit d235e12f2d254d16c1df1a0f6d933af451b47334

netctl-base: Introduce helpers to add data to existing config files
There might be a need to add data to existing system wide configuration
files rather than replacing them.

Introduce begin/end headers that will be added as comment (i.e. \#) to
file using begin_header() and end_header() helpers. These headers will
wrap block of data added during project install and can be easily
removed later when needed using sed(1) address range expression.

On install (or update) prepare_file() will remove previously added data,
if file is a existing regular file or create empty file if it does not
exist. It will fail when file exists but not a regular file.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Requires: id(1), mkdir(1), ln(1), cp(1), mv(1), rm(1), readlink(1), sed(1)
-# Requires: chown(1), chmod(1), cmp(1), mktemp(1), sort(1)
+# Requires: chown(1), chmod(1), cmp(1), mktemp(1), sort(1), tr(1)
 
 # Usage: pass() [...]
 pass()
@@ -456,6 +456,55 @@ adj_rights()
 			"${owner:-not changed}" "${mode:-not changed}"
 		shift
 	done
+}
+
+NAME_UC="$(echo "$NAME" | tr '[:lower:]' '[:upper:]')"
+begin_header_str="##### BEGIN ${NAME_UC} #####"
+end_header_str="##### END ${NAME_UC} #####"
+
+# Usage: begin_header <file>
+begin_header()
+{
+	local func="${FUNCNAME:-begin_header}"
+
+	local f="${1:?missing 1st arg to ${func}() (<file>)}"
+
+	echo "$begin_header_str" >>"$f"
+}
+
+# Usage: end_header <file>
+end_header()
+{
+	local func="${FUNCNAME:-end_header}"
+
+	local f="${1:?missing 1st arg to ${func}() (<file>)}"
+
+	echo "$end_header" >>"$f"
+}
+
+# Usage: prepare_file <file>
+prepare_file()
+{
+	local func="${FUNCNAME:-prepare_file}"
+
+	local f="${1:?missing 1st arg to ${func}() (<file>)}"
+
+	if [ -e "$f" ]; then
+		# Remove block wrapped by begin/end header
+		[ -f "$f" ] || return
+		sed -n -e "/$begin_header_str/,/$end_header_str/!p" -i "$f" ||:
+	else
+		# Make sure we are not ending with '/'
+		[ -n "${f##*/}" ] || return
+
+		local d="$ROOT/${f%/*}"
+		if [ ! -d "$d" ]; then
+			[ ! -e "$d" ] && mkdir -p "$d" || return
+		fi
+
+		# Create empty file
+		: >"$f" || return
+	fi
 }
 
 ################################################################################

commit a201640a071681fb6609e660a55870054322c816

netctl-base: Set begin/end header strings after NAME variable
They are depend on this variable value and we should set header strings
only after this variable initialized. Fix end header string variable
name (end_header_str vs end_header).

Fixes: d235e12f2d25 ("netctl-base: Introduce helpers to add data to existing config files")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

install.sh
@@ -620,10 +620,6 @@ adj_rights()
 	done
 }
 
-NAME_UC="$(echo "$NAME" | tr '[:lower:]' '[:upper:]')"
-begin_header_str="##### BEGIN ${NAME_UC} #####"
-end_header_str="##### END ${NAME_UC} #####"
-
 # Usage: begin_header <file>
 begin_header()
 {
@@ -641,7 +637,7 @@ end_header()
 
 	local f="${1:?missing 1st arg to ${func}() (<file>)}"
 
-	echo "$end_header" >>"$f"
+	echo "$end_header_str" >>"$f"
 }
 
 # Usage: prepare_file <file>
@@ -687,6 +683,11 @@ SOURCE="$(cd "$SOURCE" && echo "$PWD")" &&
 [ "$SOURCE/install.sh" -ef "$0" -a -f "$SOURCE/vars-sh" ] ||
 	abort '%s: cannot find project location\n' "$prog_name"
 NAME="${SOURCE##*/}"
+NAME_UC="$(echo "$NAME" | tr '[:lower:]' '[:upper:]')"
+
+# Prepare begin/end header strings
+begin_header_str="##### BEGIN ${NAME_UC} #####"
+end_header_str="##### END ${NAME_UC} #####"
 
 # Detect if running as base
 [ ! -L "$SOURCE/install.sh" ] &&